eac74cd9b90269acb073116ed730786332f5f719
Serdar Olmez <serdarolmez0@gmail.com>
2023-10-11 14:58:58 +0200

    formulaire validator

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
+// JavaScript pour le défilement en douceur
 
+document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
